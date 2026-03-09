f2baf3a5f337527a80def94f1e55857e57a2f15b
udaysharma189 <udaysharma9027@gmail.com>
2026-03-09 10:28:37 +0530

    do while loop

--- script.js
+++ script.js
@@ -46,8 +46,16 @@
 // for(leti=1;i<=5;i++){
 //     console.log(i);
 // }
+// let i=1;
+// while(i<=5){
+//     console.log(i);
+//      i++;
+// }
 let i=1;
-while(i<=5){
+do{
     console.log(i);
-    i++;
+}
+while(i<=5);{
+     console.log(i);
+      i++;
 }
