a00c5bb2ffb368a51b265e08cba5088ed1eeec76
udaysharma189 <udaysharma9027@gmail.com>
2026-03-09 10:15:15 +0530

    condition(if/else/elseif)

--- script.js
+++ script.js
@@ -11,35 +11,35 @@
 // }
 // // operators
 // var a=100;var b=13;var c=10; 
-let day=3;
-switch(day){
-    case 1:
-        console.log("monday");
-        break;
-        case 2:
-        console.log("tuesday");
-        break;
-        case 3:
-        console.log("Wednesday");
-        break;
-        case 4:
-        console.log("thursday");
-        break;
-        case 5:
-        console.log("friday");
-        break;
-        case 6:
-        console.log("saturday");
-        break;
-        case 7:
-        default:console.log( console.error();
-        )
+// let day=3;
+// switch(day){
+//     case 1:
+//         console.log("monday");
+//         break;
+//         case 2:
+//         console.log("tuesday");
+//         break;
+//         case 3:
+//         console.log("Wednesday");
+//         break;
+//         case 4:
+//         console.log("thursday");
+//         break;
+//         case 5:
+//         console.log("friday");
+//         break;
+//         case 6:
+//         console.log("saturday");
+//         break;
+//         case 7:
+//         default:console.log( console.error();
+//         )
 
+// }
+let marks= 95;
+if (marks>=50){
+    console.log(pass)
 }
-// let marks= 95;
-// if (marks>=50){
-//     console.log(pass)
-// }
-// else{
-//     console.log(fail)
-// }
+else{
+    console.log(fail)
+}
