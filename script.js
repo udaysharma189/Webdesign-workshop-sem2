a048d018c3139d2cedfb594a5ea55ed317bf7291
udaysharma189 <udaysharma9027@gmail.com>
2026-03-09 10:11:57 +0530

    condition statement

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
 
-const sym1 =Symbol(4)
-console.log(sym1);
-const sym2 =Symbol(4);
-console.log(sym2);
-if(sym1=sym2){
-    console.log("true");
+// const sym1 =Symbol(4)
+// console.log(sym1);
+// const sym2 =Symbol(4);
+// console.log(sym2);
+// if(sym1=sym2){
+//     console.log("true");
+// }
+// else{
+//     console.log("false")
+// }
+// // operators
+// var a=100;var b=13;var c=10; 
+// let day=3;
+// switch(day){
+//     case 1:
+//         console.log("monday");
+//         break;
+//         case 2:
+//         console.log("tuesday");
+//         break;
+//         case 3:
+//         console.log("Wednesday");
+//         break;
+//         case 4:
+//         console.log("thursday");
+//         break;
+//         case 5:
+//         console.log("friday");
+//         break;
+//         case 6:
+//         console.log("saturday");
+//         break;
+//         case 7:
+//         default:console.log( console.error();
+//         )
+
+// }
+let marks= 95;
+if (marks>=50){
+    console.log(pass)
 }
 else{
-    console.log("false")
-}
-// operators
-var a=100;var b=13;var c=10; 
-let day=3;
-switch(day){
-    case 1:
-        console.log("monday");
-        break;
-        case 2:
-        console.log("tuesday");
-        break;
-        case 3:
-        console.log("Wednesday");
-        break;
-        case 4:
-        console.log("thursday");
-        break;
-        case 5:
-        console.log("friday");
-        break;
-        case 6:
-        console.log("saturday");
-        break;
-        case 7:
-        default:console.log( console.error();
-        )
-
+    console.log(fail)
 }
