b602410adc40a84f3ed3c81f88bf0e4799c55eb7
udaysharma189 <udaysharma9027@gmail.com>
2026-03-09 10:06:59 +0530

    switch case

--- script.js
+++ script.js
@@ -11,3 +11,28 @@ else{
 }
 // operators
 var a=100;var b=13;var c=10; 
+let day=3;
+switch(day){
+    case 1:
+        console.log("monday");
+        break;
+        case 2:
+        console.log("tuesday");
+        break;
+        case 3:
+        console.log("Wednesday");
+        break;
+        case 4:
+        console.log("thursday");
+        break;
+        case 5:
+        console.log("friday");
+        break;
+        case 6:
+        console.log("saturday");
+        break;
+        case 7:
+        default:console.log( console.error();
+        )
+
+}
